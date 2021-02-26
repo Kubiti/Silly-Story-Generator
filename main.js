848141c7375f3ee1780a45ecd0f781e31d38aa15
Kubiti Samuel <kubitisam18@gmail.com>
2021-02-26 21:01:11 +0000

    HTML additions

--- main.js
+++ main.js
@@ -9,15 +9,6 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-
-// 2. RAW TEXT STRINGS
-
-/*/ 2a. Story string using template literals 
-let storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`,
-*/
-
-
-
 let storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 `, 
 insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'],
